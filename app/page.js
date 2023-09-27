7bd0fd8ed34debb570479aa5eb1508c6d6480f92
Azamat Salamatov <azamat.swift@icloud.com>
2023-09-27 15:13:22 -0500

    started project

--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
 import Image from 'next/image'
 
+
+// static metadata
+export const metadata = {
+  title: 'Home',
+};
+/**
+ *Output
+ * <head>
+ *    <title>Home</title>
+ * </head>
+ *
+ */
+
+// dynamic metadata
+export async function generateMetadata({ params, searchParams }) {
+  const product = await getProduct(params.id);
+  return {title: product.title}
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ app/posts/loading.js
@@ -0,0 +1,6 @@
+const Loading = () => {
+  // u can add any ui inside loading,m include a sckeleton
+  return <LoadingSkeleton />
+}
+
+export default Loading;
